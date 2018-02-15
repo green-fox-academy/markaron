8bd872f0967780433488b3d31496ff768c88c03e
Mark Kovacs <drmarkaron@gmail.com>
2018-02-15 15:38:15 +0100

    feat(markaron): practice

---
+++ week-01/day-04/src/FavouriteNumber.java
@@ -0,0 +1,6 @@
+public class FavouriteNumber {
+    public static void main(String[]args){
+        int fNum = 2342;
+        System.out.println("My lucky number is " + fNum);
+    }
+}
---
+++ week-01/day-04/src/Swap.java
@@ -0,0 +1,14 @@
+public class Swap {
+    public static void main (String [] args){
+        int a = 123;
+        int b = 523;
+        int c;
+
+        c = a;
+        a = b;
+        b = c;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
