f28d5ad94fecdb1260e907f272cc0b795d37bf5e
Mark Kovacs <drmarkaron@gmail.com>
2018-02-16 14:53:58 +0100

    feat(markaron): day 5 update

---
+++ week-01/day-04/src/IWontCheatOnTheExams.java
@@ -0,0 +1,12 @@
+
+public class IWontCheatOnTheExams {
+    //"I won't cheat on the exam!"
+
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 100) {
+            System.out.println("I won't cheat on the exam!" + i);
+            i++;
+        }
+    }
+}
