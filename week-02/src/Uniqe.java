0a80318b61279a52a5da374b60e1039f477fcdab
Mark Kovacs <drmarkaron@gmail.com>
2018-02-19 17:38:39 +0100

    feat(Uniqe):upload-not finished

---
+++ week-02/src/Uniqe.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Uniqe {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How many numbers do you have for me today?");
+        int numSize = scanner.nextInt();
+        int[] numArray = new int[numSize];
+
+        for (int i = 0; i < numArray.length; i++) {
+            System.out.println("Enter the next number:");
+            numArray[i] = scanner.nextInt();
+
+//            System.out.println(unique(new int[]{1, 11, 34, 11, 52, 61, 1, 34}));
+
+    }
+
+    public void int uniqe (int[] num) {
+
+        }
+
+    }
