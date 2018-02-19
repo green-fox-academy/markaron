7079236ce03d1fd80fa0d04e6f66bfa2669c89bb
Mark Kovacs <drmarkaron@gmail.com>
2018-02-19 14:48:01 +0100

    feat(Printer): upload

--- week-02/src/Printer.java
+++ week-02/src/Printer.java
@@ -1,7 +1,14 @@
 public class Printer {
     public static void main(String[] args) {
         String [] wordsToPrint = {"first", "Second","Third","Fourth"};
-        //System.out.println(printer(wordsToPrint));
+        printer(wordsToPrint);
+    }
+
+    public static void printer(String...words){
+        for(String any : words ) {
+            System.out.println(any);
+        }
+
     }
 
 }
