3c52f3fa66a418e5500999e8974c2eabc983395f
Mark Kovacs <drmarkaron@gmail.com>
2018-02-20 12:20:13 +0100

    feat(SimpleRepalce):full upload

--- week-02/day-02/src/SimpleReplace.java
+++ week-02/day-02/src/SimpleReplace.java
@@ -1,2 +1,10 @@
 public class SimpleReplace {
+    public static void main(String[] args) {
+        String example = "In a dishwasher far far away";
+
+        example = example.replaceAll("dishwasher", "galaxy");
+
+       // System.out.println(example.replaceAll("dishwasher", "galaxy"));
+        System.out.println(example);
+    }
 }
