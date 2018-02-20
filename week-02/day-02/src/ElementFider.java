94c917586ac424577a821a787b76ffae404195b1
Mark Kovacs <drmarkaron@gmail.com>
2018-02-20 16:20:08 +0100

    feat():full upload

---
+++ week-02/day-02/src/ElementFider.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class ElementFider {
+    public static void main(String[] args) {
+        System.out.println(containsSeven(arrayList));
+        // Write a method that checks if the arrayList contains "7" if it contains return "Hoorray" otherwise return "Noooooo"
+        // The output should be: "Noooooo"
+        // Do this again with a different solution using different list methods!
+    }
+
+    public static ArrayList<int> containsSeven(ArrayList<int> input){
+        if ()
+    }
+}
