3e27dd88d1a4a881b3514a03ed07c815863fe788
Mark Kovacs <drmarkaron@gmail.com>
2018-02-21 12:33:04 +0100

    feat(PurpleSteps): finish

---
+++ week-02/day-03/src/PurpleSteps.java
@@ -0,0 +1,34 @@
+import javax.swing.*;
+import java.awt.*;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class PurpleSteps {
+    public static void mainDraw(Graphics graphics) {
+        for (int i = 1; i < 20; i++) {
+            graphics.setColor(Color.MAGENTA);
+            graphics.fillRect(i*20,i*20,20,20);
+
+        }
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 343;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        jFrame.add(new ImagePanel());
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
