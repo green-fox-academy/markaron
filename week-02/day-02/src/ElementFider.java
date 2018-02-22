f3457d6f1da64ede119a8d0638851aabe9f4a4f4
Mark Kovacs <drmarkaron@gmail.com>
2018-02-22 10:20:45 +0100

    feat(ElementFinder) finish, IsInList in progress

--- week-02/day-02/src/ElementFider.java
+++ week-02/day-02/src/ElementFider.java
@@ -1,14 +1,32 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+// Write a method that checks if the arrayList contains "7" if it contains return "Hoorray" otherwise return "Noooooo"
+// The output should be: "Noooooo"
+// Do this again with a different solution using different list methods!
 
 public class ElementFider {
     public static void main(String[] args) {
-        System.out.println(containsSeven(arrayList));
-        // Write a method that checks if the arrayList contains "7" if it contains return "Hoorray" otherwise return "Noooooo"
-        // The output should be: "Noooooo"
-        // Do this again with a different solution using different list methods!
+        ArrayList<Integer> arrayList = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,8));
+
+       containsSeven(arrayList);
+        isSevenAlive(arrayList);
     }
 
-    public static ArrayList<int> containsSeven(ArrayList<int> input){
-        if ()
+    public static void containsSeven(ArrayList<Integer> input){
+        if (input.contains(7)){
+            System.out.println("Hoorray");
+        } else {
+            System.out.println("Nooooooooooo");
+        }
+    }
+
+    public static void isSevenAlive (ArrayList<Integer> input){
+        for (int i = 0; i < input.size(); i++) {
+            if (input.get(i) == 7){
+                System.out.println("Hoorray");
+            }else {
+                System.out.println("NOOOOO NOOOO");
+            }
+        }
     }
 }
