3c7fea257384859ec4dc4e93688472cf47f2e043
Mark Kovacs <drmarkaron@gmail.com>
2018-02-22 17:11:22 +0100

    feat(Uniqe): work in progress

--- week-02/src/Uniqe.java
+++ week-02/src/Uniqe.java
@@ -1,22 +1,32 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Uniqe {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("How many numbers do you have for me today?");
-        int numSize = scanner.nextInt();
-        int[] numArray = new int[numSize];
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
 
-        for (int i = 0; i < numArray.length; i++) {
-            System.out.println("Enter the next number:");
-            numArray[i] = scanner.nextInt();
+//    System.out.println("How many numbers do you have for me today?");
+//
+//    int numSize = scanner.nextInt();
+//    int[] numArray = new int[numSize];
+//
+//    for (int i = 0; i < numArray.length; i++) {
+//      System.out.println("Enter the next number:");
+//      numArray[i] = scanner.nextInt();
+//    }
 
-//            System.out.println(unique(new int[]{1, 11, 34, 11, 52, 61, 1, 34}));
+    List<Integer> numArray = Arrays.asList(1, 11, 34, 11, 52, 61, 1, 34);
+    //  System.out.println(uniqe(numArray));
+    //  Collections.sort(numArray);
+    //  System.out.println((numArray));
+  }
 
-    }
+  public static List<Integer> uniqe(List<Integer> num) {
+    Collections.sort(num);
+    List<Integer> duplicateFreeList = Arrays.asList();
 
-    public void int uniqe (int[] num) {
+    
 
-        }
 
-    }
+  }
+
+}
