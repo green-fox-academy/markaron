3a717a2d950689311dae3d7708f840a61cf14991
Mark Kovacs <drmarkaron@gmail.com>
2018-02-23 11:23:26 +0100

    feat(SuperHexagon): user defined location for the hex

--- week-02/day-05/src/SuperHexagon.java
+++ week-02/day-05/src/SuperHexagon.java
@@ -1,15 +1,25 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.Scanner;
+
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 public class SuperHexagon {
   public static void mainDraw(Graphics graphics){
-    hexaDraw(graphics);
+    // System.out.println("Where is the hexagon?");
+    // Scanner input = new Scanner(System.in);
+    // int offsetX = input.nextInt();
+    // int offsetY = input.nextInt();
+    int offsetX = 0;
+    int offsetY = 0;
+    hexaDraw(graphics,offsetX, offsetY);
   }
 
-  public static void hexaDraw(Graphics graphics){
-    int [] xPoints = {WIDTH/2 - 50, WIDTH/2 + 50, WIDTH/2 + 100, WIDTH/2 + 50, WIDTH/2 - 50, WIDTH/2 - 100};
-    int [] yPoints = {HEIGHT/2 - 86, HEIGHT/2 - 86, HEIGHT/2, HEIGHT/2 +86, HEIGHT/2 + 86, HEIGHT/2 };
+  public static void hexaDraw(Graphics graphics, int offsetX, int offsetY){
+    int [] xPoints = {WIDTH/2 - 50 + offsetX, WIDTH/2 + 50 + offsetX, WIDTH/2 + 100 + offsetX, WIDTH/2 + 50 + offsetX,
+            WIDTH/2 - 50 + offsetX, WIDTH/2 - 100 + offsetX};
+    int [] yPoints = {HEIGHT/2 - 86 + offsetY, HEIGHT/2 - 86 + offsetY, HEIGHT/2 + offsetY, HEIGHT/2 +86 + offsetY,
+            HEIGHT/2 + 86 + offsetY, HEIGHT/2 + offsetY};
     int numPonts = 6;
     graphics.drawPolygon(xPoints, yPoints, numPonts);
 
