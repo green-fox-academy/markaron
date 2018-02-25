ffebff1178f5b57d9b550b8be5d45f4d35a14ef0
Mark Kovacs <drmarkaron@gmail.com>
2018-02-25 21:11:22 +0100

    feat(JosephusProblem): second wave of counting the remaining soldiers

--- week-02/day-02/src/JosephusProblem.java
+++ week-02/day-02/src/JosephusProblem.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
@@ -5,13 +6,18 @@ import java.util.Scanner;
 public class JosephusProblem {
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
+    System.out.println("How big is the party?");
     int numOfSoldiers = input.nextInt();
     int[] array = new int[numOfSoldiers];
 
     for (int i = 0; i < numOfSoldiers ; i+=2) {
       array[i] = 1;
-      if (i == numOfSoldiers){
-        i = 1;
+    }
+    System.out.println(Arrays.toString(array));
+
+    if (array[numOfSoldiers-1] == 1 ){
+      for (int i = 0; i < numOfSoldiers; i++) {
+        array[i]++;
       }
     }
     System.out.println(Arrays.toString(array));
