3a64298e088ab8aa7318db0f7b68c78e653e525f
Mark Kovacs <drmarkaron@gmail.com>
2018-02-27 17:21:16 +0100

    feat(Logs): finish

--- week-03/day-01/src/Logs.java
+++ week-03/day-01/src/Logs.java
@@ -15,23 +15,53 @@ import java.util.Set;
 public class Logs {
   public static void main(String[] args) {
     String fileName = "logs.txt";
-    uniqeAddresses(fileName);
+    //uniqueAddresses(fileName);
+    getPostRatio(fileName);
   }
 
-  public static void uniqeAddresses (String fileName){
+  public static void uniqueAddresses(String fileName) {
     ArrayList<String> list = new ArrayList<>(Arrays.asList());
     Path logFile = Paths.get(fileName);
     try {
-      for (String line: Files.readAllLines(logFile)) {
-        list.add(line.substring(27,38));
+      for (String line : Files.readAllLines(logFile)) {
+        list.add(line.substring(27, 38));
       }
-      System.out.println(list);
 
       Set set = new HashSet(list);
       ArrayList uniqeList = new ArrayList(set);
       System.out.println("The unique list is: " + uniqeList);
 
-    }catch (IOException e){
+    } catch (IOException e) {
+      System.out.println(e);
+    }
+  }
+
+  public static void getPostRatio(String fileName) {
+    ArrayList<String> list = new ArrayList<>(Arrays.asList());
+    Path logFile = Paths.get(fileName);
+    try {
+      for (String line : Files.readAllLines(logFile)) {
+        list.add(line.substring(41, 45));
+      }
+      System.out.println(list);
+
+      double get = 0;
+      double post = 0;
+      for (int i = 0; i <list.size() ; i++) {
+        if(list.get(i).contains("GET ")){
+          get++;
+        }else{
+          post++;
+        }
+      }
+
+      double getRatio = get/list.size();
+      double postRatio = post/list.size();
+
+      System.out.println(getRatio);
+      System.out.println(postRatio);
+
+    } catch (IOException e) {
       System.out.println(e);
     }
   }
