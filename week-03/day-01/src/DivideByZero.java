a1777e65e145667fff9e3ca4caf29c1b81fde09b
Mark Kovacs <drmarkaron@gmail.com>
2018-02-27 11:15:25 +0100

    feat(DivideByZero): finish

---
+++ week-03/day-01/src/DivideByZero.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class DivideByZero {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    int num = input.nextInt();
+
+    System.out.println(divider(num));
+  }
+
+  public static int divider (int num){
+    try{
+      num = 10 / num;
+    } catch (ArithmeticException e){
+      System.out.println("fail");
+    }
+    return num;
+  }
+}
