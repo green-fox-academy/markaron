de80f2c81038c58f9d25a2f7c2157c21af170ff4
Mark Kovacs <drmarkaron@gmail.com>
2018-02-27 14:07:21 +0100

    feat(CountLines): finish

--- week-03/day-01/src/CountLines.java
+++ week-03/day-01/src/CountLines.java
@@ -1,7 +1,4 @@
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.LineNumberReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -10,19 +7,21 @@ public class CountLines {
   public static void main(String[] args) {
 
     String pathString = "file.txt";
-    System.out.println(countLines(pathString));
+    countLines(pathString);
 
   }
-  public static long countLines(String fileName) {
+  public static void countLines(String fileName) {
     Path file1 = Paths.get(fileName);
     long counter = 0;
     try {
-      counter = Files.lines(file1).count();
-    } catch (NoSuchFieldException | IOException e) {
-      e.printStackTrace();
-      return counter;
+      for(String line: Files.readAllLines(file1)){
+        counter++;
+      }
+      System.out.println(counter);
+      } catch (IOException e) {
+      //e.printStackTrace();
+      System.out.println("0");
     }
-    return counter;
     }
   }
 
