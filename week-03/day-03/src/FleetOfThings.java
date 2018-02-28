c96d524a2e83c492c5d7a1aa824fc471b544a17f
Mark Kovacs <drmarkaron@gmail.com>
2018-02-28 16:06:51 +0100

    feat(FleetOfThings)finish

--- week-03/day-03/src/FleetOfThings.java
+++ week-03/day-03/src/FleetOfThings.java
@@ -7,7 +7,16 @@ public class FleetOfThings {
     // 2. [ ] Remove the obstacles
     // 3. [x] Stand up
     // 4. [x] Eat lunch
-
+    Thing myThing1 = new Thing("Get milk");
+    Thing myThing2 = new Thing("Remove the obstacles");
+    Thing myThing3 = new Thing("Stand up");
+    Thing myThing4 = new Thing("Eat lunch");
+    fleet.add(myThing1);
+    fleet.add(myThing2);
+    fleet.add(myThing3);
+    fleet.add(myThing4);
+    myThing3.complete();
+    myThing4.complete();
     System.out.println(fleet);
   }
 }
