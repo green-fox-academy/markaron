ba82fb2183b71f072308c170f5b04a72f1307c66
Mark Kovacs <drmarkaron@gmail.com>
2018-02-28 16:25:53 +0100

    feat(Sharpie)finish

---
+++ week-03/day-03/src/Sharpie.java
@@ -0,0 +1,13 @@
+public class Sharpie {
+  String color;
+  float width;
+  float inkAmount;
+
+  public Sharpie (String color, float width){
+    inkAmount = 100;
+  }
+
+  public void use(){
+    inkAmount--;
+  }
+}
