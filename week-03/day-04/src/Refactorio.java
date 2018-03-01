1a0e4da29cbc61182bb11d2c408341d0a75d71c4
Mark Kovacs <drmarkaron@gmail.com>
2018-03-01 16:00:10 +0100

    feat(Refactorio) finish

---
+++ week-03/day-04/src/Refactorio.java
@@ -0,0 +1,14 @@
+public class Refactorio {
+  public static void main(String[] args) {
+    Integer num= 14;
+    System.out.println(factorial(num));
+  }
+
+  public static int factorial (Integer num){
+    if (num == 0){
+      return 1;
+    }else {
+      return num * factorial(num-1);
+    }
+  }
+}
