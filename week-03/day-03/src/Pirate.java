21a68c0f1b1ab087500c45afd187474615249fb6
Mark Kovacs <drmarkaron@gmail.com>
2018-03-01 17:32:17 +0100

    feat(Pirates): pirate class-fight in progress

--- week-03/day-03/src/Pirate.java
+++ week-03/day-03/src/Pirate.java
@@ -1,22 +1,38 @@
 public class Pirate {
   int intToxication;
   boolean passOut;
+  boolean dead;
 
-  public Pirate(int intToxication, double passOut) {
+  public Pirate(int intToxication, boolean passOut, boolean dead) {
     this.intToxication = intToxication;
     this.passOut = passOut;
+    this.dead = dead;
   }
 
-  public static void drinkSomeRum(int intToxication){
-    intToxication++;
+  public static void drinkSomeRum(Pirate pirate, int intToxication) {
+    if (pirate.dead) {
+      System.out.println("he's dead");
+    } else {
+      intToxication++;
+    }
   }
 
   public static void howsItGoingMate(Pirate pirate){
-    if (pirate.intToxication < 4) {
+    if(pirate.dead){
+      System.out.println("he's dead");
+    } else if (pirate.intToxication < 4) {
       System.out.println("Pour me anudder!");
     }else{
       System.out.println("Arghh, I'ma Pirate. How d'ya d'ink its goin?");
       pirate.passOut = true;
     }
+  }
+
+  public static boolean die(Pirate pirate){
+    return pirate.dead = true;
+  }
+
+  public static void brawl (Pirate pirate){
+    if 
   }
 }
