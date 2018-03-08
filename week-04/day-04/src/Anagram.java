e02aebf1dc3e7de593d7723ff51de973bf5d5b57
Mark Kovacs <drmarkaron@gmail.com>
2018-03-08 13:01:47 +0100

    feat(Anagram): start exercise

---
+++ week-04/day-04/src/Anagram.java
@@ -0,0 +1,12 @@
+public class Anagram {
+
+  public boolean anagramChecker(String word1, String word2){
+    if (sameLengthCheck(word1,word2)){
+
+    }
+  }
+
+  public boolean sameLengthCheck(String word1, String word2){
+    return (word1.length() == word2.length());
+  }
+}
