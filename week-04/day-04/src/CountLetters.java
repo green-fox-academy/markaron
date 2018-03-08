979260c81cd1ffa05ce2ef24e3ea77f101d6f801
Mark Kovacs <drmarkaron@gmail.com>
2018-03-08 17:38:16 +0100

    feat(CountLetters): test is in work in progress state

--- week-04/day-04/src/CountLetters.java
+++ week-04/day-04/src/CountLetters.java
@@ -1,18 +1,21 @@
-import org.omg.PortableInterceptor.INACTIVE;
-
 import java.util.HashMap;
 import java.util.Map;
 
 public class CountLetters {
+  public static void main(String[] args) {
+    String theWord = "albatross";
+    System.out.println(dictionaryFromWord(theWord).toString());
+    System.out.println(dictionaryFromWord(theWord).get(2));
+  }
 
-  private Map<Integer,Character> dictionaryFromWord (String theWord) {
+
+  public static Map<Integer,Character> dictionaryFromWord (String theWord) {
     Map<Integer,Character> dictionary = new HashMap<Integer, Character>();
     for (int i = 0; i <theWord.length() ; i++) {
       Integer key = i;
       char value = theWord.charAt(i);
       dictionary.put(key,value);
     }
-
-    return null;
+    return dictionary;
   }
 }
---
+++ week-04/day-04/test/CountLettersTest.java
@@ -0,0 +1,21 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CountLettersTest {
+
+  CountLetters countLetters;
+  String word;
+
+  @Before
+  public void setUp() throws Exception {
+    word = "albatross";
+    countLetters = new CountLetters();
+  }
+
+  @Test
+  public void dictionaryFromWord() {
+    assertEquals('b',countLetters.dictionaryFromWord(word).get(2));
+  }
+}
