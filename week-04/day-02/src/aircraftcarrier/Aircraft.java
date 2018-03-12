41045408800f745542871c7a1de783b4e9a41e65
Mark Kovacs <drmarkaron@gmail.com>
2018-03-12 19:01:48 +0100

    feat(Aircraft exercise): Aircraft class and subclass done, F35 is not started yet

--- week-04/day-02/src/aircraftcarrier/Aircraft.java
+++ week-04/day-02/src/aircraftcarrier/Aircraft.java
@@ -4,4 +4,31 @@ public class Aircraft {
   int currentAmmo = 0;
   int maxAmmo;
   int baseDmg;
+  int dmgDealt;
+  String typeOfPlane;
+
+  public int fight(){
+    this.currentAmmo = 0;
+    return dmgDealt = this.baseDmg * this.maxAmmo;
+  }
+
+  public int refill(int refillAmmo){
+    refillAmmo = refillAmmo - this.currentAmmo;
+    this.currentAmmo = this.maxAmmo;
+    return refillAmmo;
+  }
+
+  public String getType(){
+    return this.typeOfPlane;
+  }
+
+  public String getStatus(){
+    String status;
+    return status = "Type " + this.typeOfPlane + ", Ammo: " + this.currentAmmo + ", Base Damage: " + this.baseDmg +
+            ", All damage: " + this.baseDmg*this.maxAmmo;
+  }
+
+  public boolean isPriority(){
+    return this.typeOfPlane.equals("F35");
+  }
 }
--- week-04/day-02/src/aircraftcarrier/F16.java
+++ week-04/day-02/src/aircraftcarrier/F16.java
@@ -1,5 +1,11 @@
 package aircraftcarrier;
 
 public class F16 extends Aircraft{
-  
+
+  public F16() {
+    super.currentAmmo = 0;
+    super.maxAmmo = 8;
+    super.baseDmg = 30;
+    super.typeOfPlane = "F16";
+  }
 }
