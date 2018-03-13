393a46cdfe2b1d3488235d1993a0e19a495109c0
Mark Kovacs <drmarkaron@gmail.com>
2018-03-13 14:04:12 +0100

    feat(Aircraft exercise):exercise finished

--- week-04/day-02/src/aircraftcarrier/AircraftMain.java
+++ week-04/day-02/src/aircraftcarrier/AircraftMain.java
@@ -2,11 +2,13 @@ package aircraftcarrier;
 
 public class AircraftMain {
   public static void main(String[] args) {
-    Carrier carrierRedGun = new Carrier(25,1000);
+    Carrier carrierRedGun = new Carrier(200,1000);
     Carrier carrierBluePistol = new Carrier(2500,800);
 
     carrierRedGun.addAircraft("F16");
     carrierRedGun.addAircraft("F16");
+    carrierRedGun.addAircraft("F35");
+    carrierRedGun.addAircraft("F35");
     carrierRedGun.addAircraft("F35");
     carrierRedGun.fill();
 
