b2174ba6e094405c5a25324b7e7787a6347e6c51
Mark Kovacs <drmarkaron@gmail.com>
2018-04-03 14:35:58 +0200

    feat(hello word app): web greet app works

--- week-06/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
+++ week-06/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -3,13 +3,19 @@ package com.greenfoxacademy.springstart.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 @Controller
 public class HelloWebController {
 
+  AtomicLong id = new AtomicLong();
+
   @RequestMapping("/web/greeting")
-  public String greeting(Model model) {
-    model.addAttribute("name", " Yekatyerin");
+  public String greeting(Model model, @RequestParam(name = "name") String name) {
+    model.addAttribute("name",  name);
+    model.addAttribute("id", id.addAndGet(1));
     return "greeting";
   }
 }
