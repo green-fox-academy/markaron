d294aa48a505fa0205ab10bd17bdd921f4fa093f
Mark Kovacs <drmarkaron@gmail.com>
2018-04-03 12:36:17 +0200

    feat(hello app): greeting works

---
+++ week-06/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -0,0 +1,14 @@
+package com.greenfoxacademy.springstart.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloRESTController {
+
+  @RequestParam(value = "/greeting")
+  public Greeting greeting(){
+    return new Greeting(1,"Hi");
+  }
+}
