3d67944790f5af69242f760286e51f341cdb57e3
Mark Kovacs <drmarkaron@gmail.com>
2018-04-03 16:39:19 +0200

    feat(hello word app): random colors with javascript

--- week-06/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloToAllTheWorld.java
+++ week-06/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloToAllTheWorld.java
@@ -5,19 +5,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Random;
 import java.util.concurrent.atomic.AtomicLong;
 
 @Controller
 public class HelloToAllTheWorld {
   AtomicLong id = new AtomicLong();
-
+  Greeting greeting = new Greeting();
 
   @RequestMapping("web/greettheworld")
   public String greetTheWorld(Model model, @RequestParam(name = "name") String name){
+    model.addAttribute("color", "#A7C6D8");
     model.addAttribute("hello", new Greeting());
+    model.addAttribute("hellos", greeting.getHellos());
     model.addAttribute("name",  name);
     model.addAttribute("id", id.addAndGet(1));
-    return "greettheworld";
+    return "greetworld";
 
   }
 
