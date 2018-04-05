d0d39fe8aa77c4d767156a33fc9247d704353c56
Mark Kovacs <drmarkaron@gmail.com>
2018-04-05 11:54:42 +0200

    (dependencies training): hello printer

--- week-07/day-04/src/main/java/com/greenfoxacademy/dependency/DependencyApplication.java
+++ week-07/day-04/src/main/java/com/greenfoxacademy/dependency/DependencyApplication.java
@@ -1,12 +1,23 @@
 package com.greenfoxacademy.dependency;
 
+import com.greenfoxacademy.dependency.hellobeanworld.Printer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DependencyApplication {
+public class DependencyApplication implements CommandLineRunner{
+
+	@Autowired
+	Printer printer;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DependencyApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		printer.log("hello");
 	}
 }
