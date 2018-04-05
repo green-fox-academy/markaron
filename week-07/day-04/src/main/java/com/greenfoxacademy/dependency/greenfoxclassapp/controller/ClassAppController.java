c5a0d8216ced9f69217fe3943effdc3dc0c185e2
Mark Kovacs <drmarkaron@gmail.com>
2018-04-05 20:57:26 +0200

    feat(class app) adding to list works

--- week-07/day-04/src/main/java/com/greenfoxacademy/dependency/greenfoxclassapp/controller/ClassAppController.java
+++ week-07/day-04/src/main/java/com/greenfoxacademy/dependency/greenfoxclassapp/controller/ClassAppController.java
@@ -1,10 +1,14 @@
 package com.greenfoxacademy.dependency.greenfoxclassapp.controller;
 
 import com.greenfoxacademy.dependency.greenfoxclassapp.model.StudentService;
+import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ClassAppController {
@@ -21,5 +25,16 @@ public class ClassAppController {
   public String getList(Model model){
     model.addAttribute("list",studentService.findAll());
     return "list";
+  }
+
+  @GetMapping("/gfa/add")
+  public String addStudent(){
+    return "add";
+  }
+
+  @PostMapping("/saver")
+  public String getSaver(@ModelAttribute(name = "kid") String kidname, Model model){
+    studentService.save(kidname);
+    return "save";
   }
 }
