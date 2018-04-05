1a7a58dc13ddf0754271acdc6041f8f96ba539cc
Mark Kovacs <drmarkaron@gmail.com>
2018-04-05 20:20:57 +0200

    feat(class app) list out the students

--- week-07/day-04/src/main/java/com/greenfoxacademy/dependency/greenfoxclassapp/controller/ClassAppController.java
+++ week-07/day-04/src/main/java/com/greenfoxacademy/dependency/greenfoxclassapp/controller/ClassAppController.java
@@ -1,4 +1,25 @@
 package com.greenfoxacademy.dependency.greenfoxclassapp.controller;
 
+import com.greenfoxacademy.dependency.greenfoxclassapp.model.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class ClassAppController {
+
+  @Autowired
+  StudentService studentService;
+
+  @GetMapping("/gfa")
+  public String mainLink(){
+    return "gfa";
+  }
+
+  @GetMapping("/gfa/list")
+  public String getList(Model model){
+    model.addAttribute("list",studentService.findAll());
+    return "list";
+  }
 }
