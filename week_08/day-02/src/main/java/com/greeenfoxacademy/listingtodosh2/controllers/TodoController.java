ca5f0fcadee58c2dae5e87283be7669a8763be52
Mark Kovacs <drmarkaron@gmail.com>
2018-04-10 11:39:27 +0200

    feat(ToDo app with MySQL) list returns text

---
+++ week_08/day-02/src/main/java/com/greeenfoxacademy/listingtodosh2/controllers/TodoController.java
@@ -0,0 +1,17 @@
+package com.greeenfoxacademy.listingtodosh2.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TodoController {
+
+  @GetMapping(value = {"/todo", "/todo/list"})
+  @ResponseBody
+  public String list(Model model){
+    return "This is my first todo";
+  }
+}
+
---
+++ week_08/day-02/src/main/java/com/greeenfoxacademy/listingtodosh2/model/ToDo.java
@@ -0,0 +1,8 @@
+package com.greeenfoxacademy.listingtodosh2.model;
+
+public class ToDo {
+  private Long id;
+  private String title;
+  private boolean urgent = false;
+  private boolean done = false;
+}
