593285ae1325f8d9fdb545c54f052ff263ee4bc4
Mark Kovacs <drmarkaron@gmail.com>
2018-04-10 15:52:03 +0200

    (todo app with mysql) adding todo works, list table design done on todo page

--- week_08/day-02/secondproject/src/main/java/com/greenfoxacademy/secondproject/controller/TodoController.java
+++ week_08/day-02/secondproject/src/main/java/com/greenfoxacademy/secondproject/controller/TodoController.java
@@ -1,11 +1,16 @@
 package com.greenfoxacademy.secondproject.controller;
 
+import com.greenfoxacademy.secondproject.model.Todo;
 import com.greenfoxacademy.secondproject.repository.Repository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.annotation.PostConstruct;
 
 @Controller
 public class TodoController {
@@ -21,6 +26,17 @@ public class TodoController {
       model.addAttribute("todos", repo.findAll());
     }
     return "todo";
+  }
+
+  @GetMapping(value = "/todo/adder")
+  public String addTodoPage(){
+    return "adder";
+  }
+
+  @PostMapping("/todo/adder")
+  public String addTodo(@ModelAttribute(name = "todoDesc") String todoDescription){
+    repo.save(new Todo(todoDescription));
+    return "redirect:/todo";
   }
 }
 
